55fa191a8c7b5e4285563915b44c7afedc12ca9d
rahul <rahul@LAPTOP-T6G63HRD>
2022-10-06 09:55:52 +0530

    Add project files.

---
+++ EventDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+
+
+
+
+
+
+    namespace ClassDemos
+    {
+
+        public delegate void MyDelegate();
+        public class Stud
+        {
+            public event MyDelegate Pass; // pass & fail are the events
+            public event MyDelegate Fail;
+            public void AcceptMarks(int mark)
+            {
+                if (mark > 75)
+                {
+                    Pass(); // call to event --> raise an event
+                }
+                else if (mark < 40)
+                {
+                    Fail();
+                }
+                Console.WriteLine($"Your score is {mark}");
+            }
+        }
+
+        public class Program
+        {
+            static void Success()
+            {
+                Console.WriteLine("You got Grade A");
+            }
+            static void Fail()
+            {
+                Console.WriteLine("You are Fail");
+            }
+            static void Main(string[] args)
+            {
+
+                Stud s1 = new Stud();
+                s1.Pass += new MyDelegate(Success);
+                s1.Fail += new MyDelegate(Fail);
+                s1.AcceptMarks(85);
+
+            }
+
+        }
+
+    }
+}
+    
+
+
+
+
+
+    
+
